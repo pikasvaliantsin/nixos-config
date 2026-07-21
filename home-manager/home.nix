553f6ade6e264542199b325ebb6e20095a67e5de
Valentin Pikas <pikasvaliantsin@github.com>
2026-07-21 18:40:41 +0300

    Fix Yazi syntax: force=true outside text block

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -77,17 +77,19 @@
   };
 
   # Yazi
-  xdg.configFile."yazi/yazi.toml".text = ''
-    [opener]
-    edit = [
-      { run = 'micro "$@"', block = true }
-    ]
-    
-    [manager]
-    show_hidden = true
-    sort_by = "natural"
-    force = true
-  '';
+  xdg.configFile."yazi/yazi.toml" = {
+    text = ''
+      [opener]
+      edit = [
+        { run = 'micro "$@"', block = true }
+      ]
+      
+      [manager]
+      show_hidden = true
+      sort_by = "natural"
+    '';
+    force = true;
+  };
   	
 
   # Alacritty
