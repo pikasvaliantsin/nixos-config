d8d6380ec862e19f92609c5ff103a64e9d473c54
Valentin Pikas <pikasvaliantsin@github.com>
2026-07-22 19:51:26 +0300

    Force overwrite jetbrainsd.desktop in Home Manager

--- home-manager/modules/jetbrains.nix
+++ home-manager/modules/jetbrains.nix
@@ -1,26 +1,24 @@
 { config, pkgs, ... }:
 
 let
-  # Определяем путь к jetbrains-toolbox (unstable)
   toolbox = pkgs.jetbrains-toolbox;
 in
 {
-  # 1. Принудительно переопределяем .desktop файл
-  xdg.dataFile."applications/jetbrainsd.desktop".text = ''
-    [Desktop Entry]
-    Version=1.5
-    Name=JetBrains
-    Type=Application
-    NoDisplay=true
-    MimeType=x-scheme-handler/jetbrains;
-    Exec=${toolbox}/bin/jetbrains-toolbox %u
-  '';
-
-  # 2. Регистрируем MIME-тип для этого файла
-  xdg.mime.defaultApplications = {
-    "x-scheme-handler/jetbrains" = "jetbrainsd.desktop";
+  xdg.dataFile."applications/jetbrainsd.desktop" = {
+    text = ''
+      [Desktop Entry]
+      Version=1.5
+      Name=JetBrains
+      Type=Application
+      NoDisplay=true
+      MimeType=x-scheme-handler/jetbrains;
+      Exec=${toolbox}/bin/jetbrains-toolbox %u
+    '';
+    force = true;  # <-- добавляем
   };
 
-  # 3. (Опционально) Гарантируем, что Toolbox всегда установлен через unstable
-  home.packages = [ toolbox ];
+  # Если используем mimeApps, оставляем как есть
+  xdg.mimeApps.defaultApplications = {
+    "x-scheme-handler/jetbrains" = "jetbrainsd.desktop";
+  };
 }
